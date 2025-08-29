26e5ab6ed556deca93b6a08b5a14a67a57bc2ddc
samsudin <mohamadsamsudin47@gmail.com>
2025-08-29 11:50:29 +0700

    fix: icon

--- resources/js/components/app-logo-icon.tsx
+++ resources/js/components/app-logo-icon.tsx
@@ -2,7 +2,7 @@ import { SVGAttributes } from 'react';
 
 export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
     return (
-        <svg {...props} viewBox="0 0 25 24" xmlns="http://www.w3.org/2000/svg">
+        <svg {...props} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path
                 fillRule="evenodd"
                 clipRule="evenodd"
